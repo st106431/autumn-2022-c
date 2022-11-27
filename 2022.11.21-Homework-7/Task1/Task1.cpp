9b477092335a6b647fd05cac8213e2f4bdb8cfba
st106431 <113363606+st106431@users.noreply.github.com>
2022-11-27 18:25:30 +0300

    Homework-7 added

---
+++ 2022.11.21-Homework-7/Task1/Task1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    int n = 0;
+    int m = 0;
+    std::cin >> n >> m;
+    int** mas = new int* [n] { 0 };
+    for (int i = 0; i < n; ++i) 
+    {
+        mas[i] = new int[m] { 0 };
+    }
+    for (int i = 0; i < n; ++i) 
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cin >> mas[i][j];
+        }
+    }
+    std::cout << std::endl;
+
+    for(int i = 0; i < n; ++i)
+    {
+        int sum = 0;
+        for (int j = 0; j < m; ++j)
+        {
+            sum += mas[i][j];
+        }
+        std::cout << sum << " ";
+    }
+    std::cout << std::endl;
+    for (int j = 0; j < m; ++j)
+    {
+        int sum = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            sum += mas[i][j];
+        }
+        std::cout << sum << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cout << mas[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] mas[i];
+    }
+    delete[] mas;
+    return EXIT_SUCCESS;
+}
---
+++ 2022.11.21-Homework-7/Task4/Task4.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    int n = 0;
+    int m = 0;
+    std::cin >> n >> m;
+    int** mas = new int* [n] { 0 };
+    for (int i = 0; i < n; ++i)
+    {
+        mas[i] = new int[m] { 0 };
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cin >> mas[i][j];
+        }
+    }
+    int** mas_1 = new int* [n] { 0 };
+    for (int i = 0; i < n; ++i)
+    {
+        mas_1[i] = new int[m] { 0 };
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            mas_1[i][j] = mas[i][m - 1 - j];
+        }
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cout << mas_1[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] mas[i];
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] mas_1[i];
+    }
+    delete[] mas;
+    delete[] mas_1;
+    return EXIT_SUCCESS;
+}
